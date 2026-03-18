3cc38c3785c35fbf5608735f2c9f798cb1fac051
Carlos  Paez <carlospaez@192.168.1.5>
2026-03-18 17:52:35 -0500

    Fix: upload firebase-config.js to allow cloud version to connect

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+// This file is IGNORED by git to prevent credential leakage
+// We use a global variable to ensure compatibility when opening index.html directly (file://)
+window.firebaseConfig = {
+    apiKey: "AIzaSyDlrWH3p5y8JXMUVGFVx1Okpb4D5ZROPMg",
+    authDomain: "detersocial-5cbb0.firebaseapp.com",
+    projectId: "detersocial-5cbb0",
+    storageBucket: "detersocial-5cbb0.firebasestorage.app",
+    messagingSenderId: "245545063101",
+    appId: "1:245545063101:web:df0b76681cd7c934f15dd4",
+    measurementId: "G-K5KMCMB07K"
+};
